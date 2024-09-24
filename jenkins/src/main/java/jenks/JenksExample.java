0951ec17cad30a49c04a369c79cd090fed4efdd7
hanzh <hanzh@Hanz-Riya>
2024-09-24 10:24:03 +0530

    Please commit the message as jenkins

---
+++ jenkins/src/main/java/jenks/JenksExample.java
@@ -0,0 +1,14 @@
+package jenks;
+
+public class JenksExample 
+{
+
+	public static void main(String[] args) 
+	{
+		
+	        System.out.println("Hello World!"); 
+		// TODO Auto-generated method stub
+
+	}
+
+}
